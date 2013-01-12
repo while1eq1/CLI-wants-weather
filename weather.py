92182524a6c4a5b4be2baf02929e894863397699
William Broach <while1eq1@gmail.com>
2013-01-12 18:25:05 -0500

    rewrite to support wunderground api + complete weather dictionary

--- weather.py
+++ weather.py
@@ -6,18 +6,17 @@
 #
 # Gets the weather and returns a string
 # Why? Because I want a cute weather indicator in my prompt
-# To Do? A lot of stuff
 # Requres: python-argparse
 
 import argparse 
 import urllib2
-from xml.dom.minidom import parse
+import json
 
 def main():
     parser = argparse.ArgumentParser(description='Get the weather and return a string')
     parser.add_argument('-f', action="store_true", dest='fahrenheit', default=False,
                         help='Temp in Fahrenheit (default: False)')
-    parser.add_argument('-z', action="store", dest="zipcode", default='NY',
+    parser.add_argument('-z', action="store", dest="zipcode", default='10006',
                         help='your zipcode or your city name (default: NY)')
     parser.add_argument('-w', action="store_true", dest='show_wind', default=False,
                         help='Show wind speed')
@@ -30,76 +29,154 @@ def main():
      
     args = parser.parse_args()
 
-    WURL = "http://www.google.com/ig/api?weather="
+    BASEURL = "http://api.wunderground.com/api/6c5f9a17518f02c6/"
+    WURL = BASEURL + 'conditions/q/'
+    geourl = BASEURL + "geolookup/q/%s.json" % (args.zipcode)
 
-    # fetching data
-    req = urllib2.Request(WURL + args.zipcode)
     try:
-        # Handling some URL and network Error
-        f = urllib2.urlopen(req)
+        f = urllib2.urlopen(geourl)
     except urllib2.HTTPError, urllib2.URLError:
-        print 'The URL is not reachable'
-        return 1
-    dom = parse(f)
+        print "The URL is not reachable"
+	return 1
+    
+    j = json.loads(str(f.read()))
+    state = str(j['location']['state'])
+    city = str(j['location']['city']).replace(' ','_')
 
-    # Weather dictionary
-    # note that since google has not documented their API 
-    # I could be missing some weather
-    wconditions = { 'Drizzle': "☂",
-    		    'Clear': "☉",
+    WURL += state + '/' + city + '.json'
+
+    try:
+        f = urllib2.urlopen(WURL)
+    except urllib2.HTTPError, urllib2.URLError:
+        print "The URL is not reachable"
+	return 1
+    
+    j = json.loads(urllib2.urlopen(WURL).read())
+
+    # Wunderground Full Weather dictionary
+    wconditions = { 'Light Drizzle': "☂",
+    	            'Heavy Drizzle': "☂",
+    	            'Light Thunderstorm': "⚡",
+    	            'Heavy Thunderstorm': "⚡",
+                    'Light Rain': "☔",
+                    'Heavy Rain': "☔",
                     'Chance of Rain': "☂",
+                    'Light Rain Showers': "☔",
+                    'Heavy Rain Showers': "☔",
+                    'Light Thunderstorms and Rain': "⚡ ☔",
+                    'Heavy Thunderstorms and Rain': "⚡ ☔",
+    	            'Light Rain Mist': "☂",
+    	            'Heavy Rain Mist': "☂",
+    	            'Light Spray': "☂",
+    	            'Heavy Spray': "☂",
+                    'Light Snow': "☃",
+                    'Heavy Snow': "☃",
+                    'Light Snow Showers': "☃",
+                    'Light Thunderstorms and Snow': "☃ ⚡",
+                    'Heavy Thunderstorms and Snow': "☃ ⚡",
+                    'Heavy Snow Showers': "☃",
+                    'Light Blowing Snow': "☃",
+                    'Heavy Blowing Snow': "☃",
+                    'Light Low Drifting Snow': "☃",
+                    'Heavy Low Drifting Snow': "☃",
+                    'Light Snow Blowing Snow Mist': "☃",
+                    'Heavy Snow Blowing Snow Mist': "☃",
+                    'Light Snow Grains': "☃",
+                    'Heavy Snow Grains': "☃",
+                    'Light Ice Crystals': "☃",
+                    'Heavy Ice Crystals': "☃",
+                    'Light Ice Pellets': "☃",
+                    'Heavy Ice Pellets': "☃",
+                    'Light Ice Pellet Showers': "☃",
+                    'Heavy Ice Pellet Showers': "☃",
+		    'Light Freezing Drizzle': "☂ *",
+		    'Heavy Freezing Drizzle': "☂ *",
+		    'Light Freezing Rain': "☂ *",
+		    'Heavy Freezing Rain': "☂ *",
+    	            'Light Thunderstorms and Ice Pellets': "*⚡",
+    	            'Heavy Thunderstorms and Ice Pellets': "*⚡",
+                    'Light Hail': "*",
+                    'Heavy Hail': "*",
+                    'Small Hail': "*",
+                    'Light Hail Showers': "*",
+                    'Heavy Hail Showers': "*",
+                    'Light Small Hail Showers': "*",
+                    'Heavy Small Hail Showers': "*",
+                    'Light Thunderstorms with Hail': "H⚡",
+                    'Light Thunderstorms with Hail': "H⚡",
+                    'Light Thunderstorms with Small Hail': "H⚡",
+                    'Heavy Thunderstorms with Small Hail': "H⚡",
+                    'Light Mist': "#",
+                    'Heavy Mist': "#",
+                    'Light Fog': "#",
+                    'Heavy Fog': "#",
+                    'Light Freezing Fog': "#☃",
+                    'Heavy Freezing Fog': "#☃",
+                    'Patches of Fog': "#",
+                    'Shallow Fog': "#",
+                    'Partial Fog': "#",
+                    'Light Fog Patches': "#",
+                    'Heavy Fog Patches': "#",
+                    'Smoke': "S",
+                    'Widespread Dust': "D",
+                    'Light Blowing Widespread Dust': "D",
+                    'Heavy Blowing Widespread Dust': "D",
+                    'Light Low Drifting Widespread Dust': "D",
+                    'Heavy Low Drifting Widespread Dust': "D",
+                    'Light Dust Whirls': "D",
+                    'Heavy Dust Whirls': "D",
+                    'Light Sandstorm': "SS",
+                    'Heavy Sandstorm': "SS",
+                    'Light Sand': "SS",
+                    'Heavy Sand': "SS",
+                    'Light Blowing Sand': "SS",
+                    'Heavy Blowing Sand': "SS",
+                    'Light Low Drifting Sand': "SS",
+                    'Heavy Low Drifting Sand': "SS",
+    		    'Clear': "☉",
                     'Sunny': "☼",
                     'Mostly Sunny': "☼☁",
-                    'Partly Cloudy': "☁☼",
+                    'Partly Cloudy': "☁ ☼",
                     'Mostly Cloudy': "☁",
-                    'Chance of Storm': "☁☂",
+                    'Scattered Clounds': "☁",
+                    'Chance of Storm': "☁ ☂",
                     'Showers': "☂",
-                    'Rain': "☔",
                     'Chance of Snow': "☃",
-                    'Cloudy': "☁",
-                    'Mist': "#",
                     'Storm': "☈",
                     'Thunderstorm': "⚡",
                     'Chance of Storm': "☁?",
                     'Sleet': "S",
-                    'Snow': "☃",
                     'Icy': "I",
-                    'Dust': "D",
-                    'Fog': "#",
-                    'Smoke': "S",
-                    'Haze': "H",
+                    'Light Haze': "H",
+                    'Heavy Haze': "H",
                     'Flurries': "*",
-                    'Overcast': "☁"}
+                    'Overcast': "☁",
+                    'Funnel Cloud': "Run!!!!",
+                    'Squals': "SQ"}
 
     # prepare output
-    
     weather = ""
 
     if args.show_city:
-        weather += str(dom.getElementsByTagName('city')[0].getAttribute('data')) + " "
+        weather += str(j['current_observation']['display_location']['city']) + " "
 
     if args.show_text:
-    	weather += str(dom.getElementsByTagName('condition')[0].getAttribute('data')) + " "
+    	weather += str(j['current_observation']['weather']) + " "
         
-    weather += str(wconditions.get(dom.getElementsByTagName('condition')[0].getAttribute('data'))) + " "
-
     if args.fahrenheit:
-    	weather += str(dom.getElementsByTagName('temp_f')[0].getAttribute('data'))
-        weather += '°F '
+    	weather += str(j['current_observation']['temp_f']) + '°F '
     else:
-        weather += str(dom.getElementsByTagName('temp_c')[0].getAttribute('data'))
-        weather += '°C '
+        weather += str(j['current_observation']['temp_c']) + '°C '
 
     if args.show_wind:
-    	weather += str(dom.getElementsByTagName('wind_condition')[0].getAttribute('data')).replace('Wind','≋') + " "
+    	weather += str(j['current_observation']['wind_dir']) + " " + str(j['current_observation']['wind_mph']) + " MPH "
     
     if args.show_humidity:
-    	weather += str(dom.getElementsByTagName('humidity')[0].getAttribute('data')).replace('Humidity','ϕ') 
+    	weather += str(j['current_observation']['relative_humidity']) + ' ϕ '
 
-        
+
     # and now the end
-    # print '%s %d°%s' % (wsymbol, int(temp), unit)
-    print weather
+    print wconditions[j['current_observation']['weather']] + '  ' + weather
 
     return 0
 
