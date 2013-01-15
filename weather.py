ad1765fa01e93c92ae16b48d89bc57a2a0fa9449
William Broach <while1eq1@gmail.com>
2013-01-15 10:45:33 -0500

    offloaded weather dict to its own file

--- weather.py
+++ weather.py
@@ -9,12 +9,13 @@
 # Requires: python-argparse, Wunderground API Key
 # API Key can be obtained for free here: http://www.wunderground.com/weather/api/
 
+import weatherdict
 import argparse
 import urllib2
 import json
 import sys
 
-API_KEY ="Your API Key here"
+API_KEY ="6c5f9a17518f02c6"
 
 def main():
     parser = argparse.ArgumentParser(description='Get the weather and return a string')
@@ -67,109 +68,6 @@ def main():
     alerts = json.loads(urllib2.urlopen(alertsurl).read())
     j = json.loads(urllib2.urlopen(WURL).read())
 
-    # Wunderground Full Weather dictionary
-    wconditions = { 'Light Drizzle': "☂",
-    	            'Heavy Drizzle': "☂",
-    	            'Drizzle': "☂",
-    	            'Light Thunderstorm': "⚡",
-    	            'Heavy Thunderstorm': "⚡",
-                    'Rain': "☂",
-                    'Light Rain': "☂",
-                    'Heavy Rain': "☂",
-                    'Chance of Rain': "☂",
-                    'Light Rain Showers': "☂",
-                    'Heavy Rain Showers': "☂",
-                    'Light Thunderstorms and Rain': "⚡ ☂",
-                    'Heavy Thunderstorms and Rain': "⚡ ☂",
-    	            'Light Rain Mist': "☂",
-    	            'Heavy Rain Mist': "☂",
-    	            'Light Spray': "☂",
-    	            'Heavy Spray': "☂",
-                    'Light Snow': "☃",
-                    'Heavy Snow': "☃",
-                    'Light Snow Showers': "☃",
-                    'Light Thunderstorms and Snow': "☃ ⚡",
-                    'Heavy Thunderstorms and Snow': "☃ ⚡",
-                    'Heavy Snow Showers': "☃",
-                    'Light Blowing Snow': "☃",
-                    'Heavy Blowing Snow': "☃",
-                    'Light Low Drifting Snow': "☃",
-                    'Heavy Low Drifting Snow': "☃",
-                    'Light Snow Blowing Snow Mist': "☃",
-                    'Heavy Snow Blowing Snow Mist': "☃",
-                    'Light Snow Grains': "☃",
-                    'Heavy Snow Grains': "☃",
-                    'Light Ice Crystals': "❄",
-                    'Heavy Ice Crystals': "❄",
-                    'Light Ice Pellets': "❄",
-                    'Heavy Ice Pellets': "❄",
-                    'Light Ice Pellet Showers': "❄",
-                    'Heavy Ice Pellet Showers': "❄",
-        		    'Light Freezing Drizzle': "☂ ❄",
-	        	    'Heavy Freezing Drizzle': "☂ ❄",
-		            'Light Freezing Rain': "☂ ❄",
-		            'Heavy Freezing Rain': "☂ ❄",
-    	            'Light Thunderstorms and Ice Pellets': "❄⚡",
-    	            'Heavy Thunderstorms and Ice Pellets': "❄⚡",
-                    'Light Hail': "☍",
-                    'Heavy Hail': "☍",
-                    'Small Hail': "☍",
-                    'Light Hail Showers': "☍",
-                    'Heavy Hail Showers': "☍",
-                    'Light Small Hail Showers': "☍",
-                    'Heavy Small Hail Showers': "☍",
-                    'Light Thunderstorms with Hail': "☍⚡",
-                    'Light Thunderstorms with Hail': "☍⚡",
-                    'Light Thunderstorms with Small Hail': "☍⚡",
-                    'Heavy Thunderstorms with Small Hail': "☍⚡",
-                    'Fog': "₣",
-                    'Light Mist': "#",
-                    'Heavy Mist': "#",
-                    'Light Fog': "₣",
-                    'Heavy Fog': "₣",
-                    'Light Freezing Fog': "₣❄",
-                    'Heavy Freezing Fog': "₣❄",
-                    'Patches of Fog': "₣",
-                    'Shallow Fog': "₣",
-                    'Partial Fog': "₣",
-                    'Light Fog Patches': "₣",
-                    'Heavy Fog Patches': "₣",
-                    'Smoke': "S",
-                    'Widespread Dust': "D",
-                    'Light Blowing Widespread Dust': "D",
-                    'Heavy Blowing Widespread Dust': "D",
-                    'Light Low Drifting Widespread Dust': "D",
-                    'Heavy Low Drifting Widespread Dust': "D",
-                    'Light Dust Whirls': "D",
-                    'Heavy Dust Whirls': "D",
-                    'Light Sandstorm': "SS",
-                    'Heavy Sandstorm': "SS",
-                    'Light Sand': "SS",
-                    'Heavy Sand': "SS",
-                    'Light Blowing Sand': "SS",
-                    'Heavy Blowing Sand': "SS",
-                    'Light Low Drifting Sand': "SS",
-                    'Heavy Low Drifting Sand': "SS",
-    		        'Clear': "☉",
-                    'Sunny': "☼",
-                    'Mostly Sunny': "☼☁",
-                    'Partly Cloudy': "☁ ☼",
-                    'Mostly Cloudy': "☁",
-                    'Scattered Clounds': "☁",
-                    'Chance of Storm': "☁ ☂",
-                    'Showers': "☂",
-                    'Chance of Snow': "☃",
-                    'Storm': "☈",
-                    'Thunderstorm': "⚡",
-                    'Sleet': "S",
-                    'Icy': "I",
-                    'Light Haze': "H",
-                    'Heavy Haze': "H",
-                    'Flurries': "*",
-                    'Overcast': "☁",
-                    'Funnel Cloud': "Run!!!!",
-                    'Squals': "SQ"}
-
     # prepare output
     weather = ""
 
@@ -193,9 +91,9 @@ def main():
 
     # and now the end
     if alerts['alerts']:
-        print wconditions[j['current_observation']['weather']] + '  ' + weather + "⚠ " + str(alerts['alerts'][0]['description']) + " ⚠ "
+        print weatherdict.wconditions[j['current_observation']['weather']] + '  ' + weather + "⚠ " + str(alerts['alerts'][0]['description']) + " ⚠ "
     else:
-        print wconditions[j['current_observation']['weather']] + '  ' + weather
+        print weatherdict.wconditions[j['current_observation']['weather']] + '  ' + weather
 
     return(0)
 
